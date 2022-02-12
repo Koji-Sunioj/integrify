3621f4e75dba0d9d4a9c03df744a1a05d28cc61c
Koji Inoue <koji.gabriel218@gmail.com>
2022-02-12 21:24:26 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import Webpages from './webpages';
-import Navbar from './webpages/navbar';
+
 function App() {
   return (
     <div className="container">
-      <Navbar/>
       <Webpages />
     </div>
   );
